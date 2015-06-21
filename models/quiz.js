c0f9ced610f364ac3357b8042b20cd24812b0c4d
chemary55 <chemary55@gmail.com>
2015-06-21 16:23:25 +0200

    Tematica 1.1

--- models/quiz.js
+++ models/quiz.js
@@ -11,6 +11,10 @@ module.exports = function(sequelize, DataTypes){
 		respuesta: {
 			type: DataTypes.STRING,
 			validate: {notEmpty: {msg: "-> Falta la respuesta"}}
+		},
+		tematica: {
+			type: DataTypes.STRING,
+			validate: {notEmpty: {msg: "-> Falta la temática"}}
 		}
 	});
 }
