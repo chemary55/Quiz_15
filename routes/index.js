09fad1ba03582c8091ec358db57fbfd9ed887a77
chemary55 <chemary55@gmail.com>
2015-06-23 20:58:53 +0200

    Moderacion de Comentarios

--- routes/index.js
+++ routes/index.js
@@ -10,8 +10,9 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz', errors: [] });
 });
 
-//Autoload de comandos con :quizId
+//Autoload de comandos con :quizId y commentId
 router.param('quizId', quizController.load); //autoload :quizId
+router.param('commentId', commentController.load);
 
 // Definición de rutas de sesion
 router.get('/login',  sessionController.new);     // formulario login
@@ -34,6 +35,9 @@ router.delete('/quizes/:quizId(\\d+)',     sessionController.loginRequired, quiz
 // Definición de rutas de comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new',   commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments',      commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', 
+	                                    sessionController.loginRequired, commentController.publish);
+
 
 router.get('/author', function(req, res)
     { res.render('author', {errors: []});
